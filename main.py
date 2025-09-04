abd817f1e1cfff3869fabc0e6d4f375a675c20cf
Neev Sharma <168625651+Neev17964@users.noreply.github.com>
2025-09-04 20:50:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,20 +11,17 @@ with open('Trained_Model', 'rb') as f:
 with open('tokenizer', 'rb') as f:
     tokenizer = cloudpickle.load(f)
 
-# Load trusted links
-trusted_df = pd.read_csv("links.csv")  # assuming one column with URLs
-trusted_urls = set(trusted_df['Official Website'].str.lower())  # convert to lowercase for uniformity
+trusted_df = pd.read_csv("links.csv")  
+trusted_urls = set(trusted_df['Official Website'].str.lower())  
 
 from keras.preprocessing.sequence import pad_sequences
 
 def hybrid_predict(url):
     url_lower = url.lower()
     
-    # Step 1: Check if URL is in trusted list
     if url_lower in trusted_urls:
         return "Safe"
     
-    # Step 2: If not, predict using the LSTM model
     seq = tokenizer.texts_to_sequences([url_lower])
     padded = pad_sequences(seq, maxlen=200)
     pred = model.predict(padded)[0][0]
@@ -37,10 +34,8 @@ def hybrid_predict(url):
 st.title("🔗 URL Safety Checker")
 st.write("Paste a URL below and check if it is safe or phishing.")
 
-# Input URL from user
 user_url = st.text_input("Enter URL here:")
 
-# Prediction button
 if st.button("Check URL"):
     if user_url:
         result = hybrid_predict(user_url)
@@ -49,4 +44,5 @@ if st.button("Check URL"):
         else:
             st.warning("Not safe url (Fraud)")
     else:
+
         st.warning("Please enter a URL to check.")
